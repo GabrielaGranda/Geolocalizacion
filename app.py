3e64c85e5a36580d5db437eab60a05c28a986410
Gabriela Granda <166565982+GabrielaGranda@users.noreply.github.com>
2025-12-09 14:22:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS 
 import time
 
 app = Flask(__name__)
+CORS(app)
 
 # Aquí guardamos las ubicaciones en memoria:
 locations = {}  # { "ABC123": {"lat":.., "lng":.., "timestamp":.. } }
